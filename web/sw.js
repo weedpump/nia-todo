6dcbecc194fe219fa00313b8ae29d06e03a5c392
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-16 23:05:24 +0200

    chore: revert dev-specific changes before release v0.4.8

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 // nia-todo Service Worker - Bulletproof Offline-First + Update-System + Push Notifications
-const SW_VERSION = 'v0.4.8-dev';
+const SW_VERSION = 'v0.4.8';
 const CACHE_NAME = 'nia-todo-' + SW_VERSION;
 const API_CACHE = 'nia-todo-api-' + SW_VERSION;
 
