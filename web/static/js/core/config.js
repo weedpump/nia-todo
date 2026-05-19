123c7dfe1721f855db14bef8b173c5332b5dd2d2
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-19 19:10:59 +0200

    refactor: modularize frontend shell

---
+++ web/static/js/core/config.js
@@ -0,0 +1,10 @@
+export const API = '';
+
+export const WS_URL = (() => {
+  const proto = location.protocol === 'https:' ? 'wss:' : 'ws:';
+  return `${proto}//${location.host}/ws`;
+})();
+
+export const DB_NAME = 'nia-todo-db';
+export const DB_VERSION = 3;
+export const APP_VERSION = 'v0.4.11-dev';
