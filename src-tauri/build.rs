41a700af2032411483fe85401644e7c99ac72b6f
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-21 13:48:29 +0200

    fix: allow tauri desktop commands from local server

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,14 @@
 fn main() {
-  tauri_build::build()
+  tauri_build::try_build(
+    tauri_build::Attributes::new().app_manifest(
+      tauri_build::AppManifest::new().commands(&[
+        "desktop_get_settings",
+        "desktop_set_setting",
+        "desktop_set_server_url",
+        "desktop_clear_server_url",
+        "desktop_notify",
+      ]),
+    ),
+  )
+  .expect("failed to run tauri-build");
 }
