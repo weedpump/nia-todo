11f5e74c858fde122945bc9f65405b68d2dd76b7
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-22 00:27:27 +0200

    chore: bump develop version to 1.6.4-dev

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 // nia-todo Service Worker - Bulletproof Offline-First + Update-System + Push Notifications
-const SW_VERSION = 'v1.6.3';
+const SW_VERSION = 'v1.6.4-dev';
 const CACHE_NAME = 'nia-todo-' + SW_VERSION;
 
 // ALLE Assets die wir brauchen
