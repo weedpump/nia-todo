9ce8ecd354391d6c62d1d5dc1b306390cbca3e56
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-21 18:38:41 +0200

    test: isolate android touch regression

--- src-tauri/gen/android/app/src/main/java/de/tobiaskneidl/nia_todo/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/de/tobiaskneidl/nia_todo/MainActivity.kt
@@ -1,37 +1,3 @@
 package de.tobiaskneidl.nia_todo
 
-import android.graphics.Color
-import android.os.Build
-import android.os.Bundle
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
-
-class MainActivity : TauriActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        applySystemBarStyle()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        applySystemBarStyle()
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) applySystemBarStyle()
-    }
-
-    private fun applySystemBarStyle() {
-        WindowCompat.setDecorFitsSystemWindows(window, true)
-        window.statusBarColor = Color.rgb(15, 23, 42)
-        window.navigationBarColor = Color.rgb(15, 23, 42)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isStatusBarContrastEnforced = false
-            window.isNavigationBarContrastEnforced = false
-        }
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
-        controller.isAppearanceLightStatusBars = false
-        controller.isAppearanceLightNavigationBars = false
-    }
-}
+class MainActivity : TauriActivity()
