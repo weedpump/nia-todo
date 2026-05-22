9bdeb390a5c5549f447997d6918b7d7f18e6170d
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-22 10:16:46 +0200

    chore: prepare release v1.7.3

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 // nia-todo Service Worker - Bulletproof Offline-First + Update-System + Push Notifications
-const SW_VERSION = 'v1.7.3-dev';
+const SW_VERSION = 'v1.7.3';
 const CACHE_NAME = 'nia-todo-' + SW_VERSION;
 
 // ALLE Assets die wir brauchen
