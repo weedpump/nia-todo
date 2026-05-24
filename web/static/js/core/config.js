9a2171029fd8634d9b0dc2c13b10a7513a3cea8f
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-24 17:34:32 +0200

    chore: prepare release v2.1.0

--- web/static/js/core/config.js
+++ web/static/js/core/config.js
@@ -2,7 +2,7 @@ export let API = '';
 export let WS_URL = websocketUrlFromBase(location.origin);
 export const DB_NAME = 'nia-todo-db';
 export const DB_VERSION = 4;
-export const APP_VERSION = 'v2.1.0-dev';
+export const APP_VERSION = 'v2.1.0';
 
 export function getTauri() {
   return window.__TAURI__ || null;
