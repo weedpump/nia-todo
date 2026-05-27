f48e2d1f3c9ffabd1c23422ecec6987faf8b7bdf
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-28 00:20:58 +0200

    show native app server address in downloads modal

--- web/static/js/core/config.js
+++ web/static/js/core/config.js
@@ -4,7 +4,7 @@ export let API = '';
 export let WS_URL = websocketUrlFromBase(location.origin);
 export const DB_NAME = 'nia-todo-db';
 export const DB_VERSION = 4;
-export const APP_VERSION = 'v2.5.22-dev';
+export const APP_VERSION = 'v2.5.23-dev';
 
 export function getTauri() {
   return window.__TAURI__ || null;
