49d8b17368bd6358e86b7392a2fb6b5201878927
Tobi <26301707+weedpump@users.noreply.github.com>
2026-05-27 23:38:59 +0200

    tune app download footer spacing

--- web/static/js/core/config.js
+++ web/static/js/core/config.js
@@ -4,7 +4,7 @@ export let API = '';
 export let WS_URL = websocketUrlFromBase(location.origin);
 export const DB_NAME = 'nia-todo-db';
 export const DB_VERSION = 4;
-export const APP_VERSION = 'v2.5.10-dev';
+export const APP_VERSION = 'v2.5.11-dev';
 
 export function getTauri() {
   return window.__TAURI__ || null;
