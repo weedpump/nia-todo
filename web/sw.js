f697f2a63f48ee0ed4b3aaa521e276de05694bb2
Tobi <26301707+weedpump@users.noreply.github.com>
2026-06-17 14:55:51 +0200

    Load scrollbar overlay reliably through service worker

--- web/sw.js
+++ web/sw.js
@@ -9,6 +9,7 @@ const PRECACHE_ASSETS = [
   '/manifest.json',
   '/static/style.css',
   '/static/js/main.js',
+  '/static/js/features/auto-scrollbars.js',
   '/static/js/app.js',
   '/static/js/api/http.js',
   '/static/js/api/errors.js',
