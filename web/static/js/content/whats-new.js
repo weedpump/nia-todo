0800d5031eb911b899154725e48c7c3e90f3c418
Tobi <26301707+weedpump@users.noreply.github.com>
2026-07-12 11:58:25 +0200

    feat: add what's new release tour

---
+++ web/static/js/content/whats-new.js
@@ -0,0 +1,26 @@
+export const WHATS_NEW_RELEASES = [
+  {
+    version: '3.0.0',
+    appVersions: ['v2.12.3-dev'],
+    badgeKey: 'whatsNew.v3.badge',
+    titleKey: 'whatsNew.v3.title',
+    introKey: 'whatsNew.v3.intro',
+    slides: [
+      {
+        icon: '✨',
+        titleKey: 'whatsNew.v3.slide1.title',
+        bodyKey: 'whatsNew.v3.slide1.body',
+      },
+      {
+        icon: '🧭',
+        titleKey: 'whatsNew.v3.slide2.title',
+        bodyKey: 'whatsNew.v3.slide2.body',
+      },
+      {
+        icon: '📎',
+        titleKey: 'whatsNew.v3.slide3.title',
+        bodyKey: 'whatsNew.v3.slide3.body',
+      },
+    ],
+  },
+];
