52933f34430c1e634b53b17af8205000959cc3bd
Tobi <26301707+weedpump@users.noreply.github.com>
2026-07-13 16:20:16 +0200

    chore: bump develop version to 3.0.3-dev

--- web/static/js/core/config.js
+++ web/static/js/core/config.js
@@ -4,7 +4,7 @@ export let API = '';
 export let WS_URL = websocketUrlFromBase(location.origin);
 export const DB_NAME = 'nia-todo-db';
 export const DB_VERSION = 4;
-export const APP_VERSION = 'v3.0.2';
+export const APP_VERSION = 'v3.0.3-dev';
 
 export function getTauri() {
   return window.__TAURI__ || null;
