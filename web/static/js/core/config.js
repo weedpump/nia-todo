09ca520d1a6ff3fd87bfe242907cceaa00593738
Tobias Kneidl <tobias@kneidl-home.de>
2026-09-12 00:44:14 +0200

    chore: prepare release v3.1.0

--- web/static/js/core/config.js
+++ web/static/js/core/config.js
@@ -7,7 +7,7 @@ export let API = '';
 export let WS_URL = websocketUrlFromBase(location.origin);
 export const DB_NAME = 'nia-todo-db';
 export const DB_VERSION = 4;
-export const APP_VERSION = 'v3.0.3-dev';
+export const APP_VERSION = 'v3.1.0';
 
 const TAURI_API = Object.freeze({
   core: Object.freeze({ invoke }),
